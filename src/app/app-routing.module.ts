54971131ea777b667418999a55e9d6f007d668d3
jLuseno161 <joyluseno2@gmail.com>
2021-05-24 06:54:02 +0300

    add path import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './component/landing/landing.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
