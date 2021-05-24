ba114328dfbf9064196f5215266c0e1708cbd2d1
jLuseno161 <joyluseno2@gmail.com>
2021-05-24 06:56:20 +0300

    imprt NotFound path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './component/landing/landing.component';
+import { NotFoundComponent } from './component/not-found/not-found.component';
 import { ProfileComponent } from './component/profile/profile.component';
 
 const routes: Routes = [
