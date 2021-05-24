4017d6371de6a397fc07c746242907124b7b7b27
jLuseno161 <joyluseno2@gmail.com>
2021-05-24 06:54:53 +0300

    import profile path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './component/landing/landing.component';
+import { ProfileComponent } from './component/profile/profile.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
